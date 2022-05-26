c903afb0fc2a04805ea2fcc8491b455939f38a23
arthurvilefort <arthur.vilefort@gmail.com>
2022-05-26 11:52:11 -0300

    novo menu

--- blog/resources/views/layouts/geral.blade.php
+++ blog/resources/views/layouts/geral.blade.php
@@ -11,10 +11,43 @@
     <title>{{ config('app.name', 'SIGECARD') }}</title>
 
 
+    <!-- Required meta tags-->
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <meta name="description" content="au theme template">
+      <meta name="author" content="Hau Nguyen">
+      <meta name="keywords" content="au theme template">
+
+      <!-- Title Page-->
+      <title>Dashboard</title>
+
+      <!-- Fontfaces CSS-->
+      <link href="css/font-face.css" rel="stylesheet" media="all">
+      <link href="css/font-awesome.min.css" rel="stylesheet" media="all">
+      <link href="css/fontawesome-all.min.css" rel="stylesheet" media="all">
+      <link href="css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+
+      <!-- Bootstrap CSS-->
+      <link href="css/bootstrap.min.css" rel="stylesheet" media="all">
+
+      <!-- Vendor CSS-->
+      <link href="css/animsition.min.css" rel="stylesheet" media="all">
+      <link href="css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+      <link href="css/animate.css" rel="stylesheet" media="all">
+      <link href="css/hamburgers.min.css" rel="stylesheet" media="all">
+      <link href="css/slick.css" rel="stylesheet" media="all">
+      <link href="css/select2.min.css" rel="stylesheet" media="all">
+      <link href="css/perfect-scrollbar.css" rel="stylesheet" media="all">
+
+      <!-- Main CSS-->
+      <link href="css/theme.css" rel="stylesheet" media="all">
+
+
+
     <!-- Styles -->
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.0-1/js/all.min.js" integrity="sha512-NBPDzmbzjlbKiCJ4gojF6V5hwwaiATqKnSRisMaVpJVECLJ10Dd88t/YV42Y8PMWBlqvIOJtXVBz79wgYlTY7w==" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <!--link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"-->
     <!--link href="{{ asset('css/app2.css') }}" rel="stylesheet">
     <link href="{{ asset('css/AdminLTE.min.css') }}" rel="stylesheet"-->
@@ -151,6 +184,114 @@
             <section class="content">
                 <div class="row">
                   <div class="col-md-3">
+
+                                    <!-- MENU SIDEBAR-->
+                 <aside class="menu-sidebar d-none d-lg-block">
+                     <div class="logo">
+                         <a href="#">
+                             <img src="/img/logo.png" alt="Cool Admin" />
+                         </a>
+                     </div>
+                     <div class="menu-sidebar__content js-scrollbar1">
+                         <nav class="navbar-sidebar">
+                             <ul class="list-unstyled navbar__list">
+                                 <li class="active has-sub">
+                                     <a class="js-arrow" href="#">
+                                         <i class="fas fa-tachometer-alt"></i>Dashboard</a>
+                                     <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                         <li>
+                                             <a href="index.html">Dashboard 1</a>
+                                         </li>
+                                         <li>
+                                             <a href="index2.html">Dashboard 2</a>
+                                         </li>
+                                         <li>
+                                             <a href="index3.html">Dashboard 3</a>
+                                         </li>
+                                         <li>
+                                             <a href="index4.html">Dashboard 4</a>
+                                         </li>
+                                     </ul>
+                                 </li>
+                                 <li>
+                                     <a href="chart.html">
+                                         <i class="fas fa-chart-bar"></i>Charts</a>
+                                 </li>
+                                 <li>
+                                     <a href="table.html">
+                                         <i class="fas fa-table"></i>Tables</a>
+                                 </li>
+                                 <li>
+                                     <a href="form.html">
+                                         <i class="far fa-check-square"></i>Forms</a>
+                                 </li>
+                                 <li>
+                                     <a href="calendar.html">
+                                         <i class="fas fa-calendar-alt"></i>Calendar</a>
+                                 </li>
+                                 <li>
+                                     <a href="map.html">
+                                         <i class="fas fa-map-marker-alt"></i>Maps</a>
+                                 </li>
+                                 <li class="has-sub">
+                                     <a class="js-arrow" href="#">
+                                         <i class="fas fa-copy"></i>Pages</a>
+                                     <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                         <li>
+                                             <a href="login.html">Login</a>
+                                         </li>
+                                         <li>
+                                             <a href="register.html">Register</a>
+                                         </li>
+                                         <li>
+                                             <a href="forget-pass.html">Forget Password</a>
+                                         </li>
+                                     </ul>
+                                 </li>
+                                 <li class="has-sub">
+                                     <a class="js-arrow" href="#">
+                                         <i class="fas fa-desktop"></i>UI Elements</a>
+                                     <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                         <li>
+                                             <a href="button.html">Button</a>
+                                         </li>
+                                         <li>
+                                             <a href="badge.html">Badges</a>
+                                         </li>
+                                         <li>
+                                             <a href="tab.html">Tabs</a>
+                                         </li>
+                                         <li>
+                                             <a href="card.html">Cards</a>
+                                         </li>
+                                         <li>
+                                             <a href="alert.html">Alerts</a>
+                                         </li>
+                                         <li>
+                                             <a href="progress-bar.html">Progress Bars</a>
+                                         </li>
+                                         <li>
+                                             <a href="modal.html">Modals</a>
+                                         </li>
+                                         <li>
+                                             <a href="switch.html">Switchs</a>
+                                         </li>
+                                         <li>
+                                             <a href="grid.html">Grids</a>
+                                         </li>
+                                         <li>
+                                             <a href="fontawesome.html">Fontawesome Icon</a>
+                                         </li>
+                                         <li>
+                                             <a href="typo.html">Typography</a>
+                                         </li>
+                                     </ul>
+                                 </li>
+                             </ul>
+                         </nav>
+                     </div>
+                 </aside>
+                 <!-- END MENU SIDEBAR-->
 
 
 
@@ -458,7 +599,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.js" integrity="sha512-jKxp7JHEN6peEmzmg6a7XJBORNTB0ITD2Pi+6FUkc16PCaNAJX2ahZ1ejn1p1uY37Pxyirn/0OMNZbITbEg3jw==" crossorigin="anonymous"></script>
     <script src="/js/popper.min.js"></script>
     <!--script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+
+
     <script src="/js/jquery.mask.min.js"></script>
     <script src="/js/geral.min.js"></script>
     <script src="/js/pnotify.custom.min.js"></script>
